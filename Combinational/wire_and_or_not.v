e4d1d5ba94bb227a967d4aa5ab51e555d4c6f779
reshmisoffi27-wq <reshmisoffi27@gmail.com>
2026-05-30 20:42:06 +0530

    Create wire_and_or_not.v
    Create two intermediate wires (named anything you want) to connect the AND and OR gates together. Note that the wire that feeds the NOT gate is really wire out, so you do not necessarily need to declare a third wire here.

---
+++ Combinational/wire_and_or_not.v
@@ -0,0 +1,12 @@
+Code : 
+module top_module (
+	input a ,
+	input b ,
+	input c ,
+	input d ,
+	output out ,
+	output out_n
+);
+	assign out = ( a & b ) | ( c & d ) ;
+	assign out_n = ~ ( ( a & b ) | ( c & d) ) ;
+endmodule
